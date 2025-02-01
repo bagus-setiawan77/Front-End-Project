4ba19ce14a6e2023f8f25367e32a642c3959d939
bagus-setiawan77 <bagus.setiawan.117788@gmail.com>
2025-02-01 20:52:13 +0700

    feat/Front-End-Project/Update_Javascript

---
+++ Music_Website/script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementById('menu');
+const close = document.getElementById('close');
+const navbar = document.getElementById('navbar');
+const header = document.getElementById("header");
+const main = document.getElementById('main');
+const playlist = document.getElementById('playlist');
+
+if(menu){
+    menu.addEventListener('click', () => {
+        navbar.classList.add('active');
+        playlist.classList.add('active');
+        header.classList.add('active');
+        main.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener("click", () => {
+        navbar.classList.remove("active");
+        playlist.classList.remove("active");
+        header.classList.remove("active");
+        main.classList.remove("active");
+    });
+}
