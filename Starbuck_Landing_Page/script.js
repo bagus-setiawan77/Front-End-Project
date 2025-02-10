9e89f1dd9a368f5524422111aca835e291733cca
bagus-setiawan77 <bagus.setiawan.117788@gmail.com>
2025-02-10 22:07:36 +0700

    feat/Starbuck_Landing_Page/Aside_JS_Add_Product

--- Starbuck_Landing_Page/script.js
+++ Starbuck_Landing_Page/script.js
@@ -1,50 +1,49 @@
-const bagOpen = document.querySelector('.bag-wrapper');
-const asideBar = document.getElementById('aside');
-const bagClose = document.querySelector('.aside-close');
+const bagOpen = document.querySelector(".bag-wrapper");
+const asideBar = document.getElementById("aside");
+const bagClose = document.querySelector(".aside-close");
 
-bagOpen.addEventListener('click', () =>{
-    asideBar.classList.add('active');
+bagOpen.addEventListener("click", () => {
+  asideBar.classList.add("active");
 });
-bagClose.addEventListener('click', () =>{
-    asideBar.classList.remove('active');
+bagClose.addEventListener("click", () => {
+  asideBar.classList.remove("active");
 });
-
 
 const plusBtn = document.querySelectorAll(".plus-btn");
-plusBtn.forEach(button =>{
-    button.addEventListener('click', event =>{
-        const menuAside = event.target.closest('.card-menu');
-        addtoMenu(menuAside);
-    });
+plusBtn.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const menuAside = event.target.closest(".card-menu");
+    addtoMenu(menuAside);
+  });
 });
 
-const asideContent = document.querySelector('.aside-content')
-const addtoMenu = menuAside => {
-    const menuImgSrc = menuAside.querySelector('img').src;
-    const menuTitle = menuAside.querySelector('.card-menu-title').textContent;
-    const menuPrice = menuAside.querySelector('.card-menu-price').textContent;
+const asideContent = document.querySelector(".aside-content");
+const addtoMenu = (menuAside) => {
+  const menuImgSrc = menuAside.querySelector("img").src;
+  const menuTitle = menuAside.querySelector(".card-menu-title").textContent;
+  const menuPrice = menuAside.querySelector(".card-menu-price").textContent;
 
-    const menuItems = asideContent.querySelectorAll('.card-menu-title');
-    for(let item of menuItems){
-        if(item === menuTitle){
-            alert('nope');
-            return;
-        }
+  const menuItems = asideContent.querySelectorAll(".card-menu-title");
+  for (let item of menuItems) {
+    if (item === menuTitle) {
+      alert("nope");
+      return;
     }
+  }
 
-    const asideMenu = document.createElement('div');
-    asideMenu.classList.add('.aside-menu');
-    asideMenu.innerHTML = `
+  const asideMenu = document.createElement("div");
+  asideMenu.classList.add(".aside-menu");
+  asideMenu.innerHTML = `
         <div class="aside-menu">
             <div class="aside-menu-img">
                 <img src="${menuImgSrc}" alt="">
             </div>
             <div class="aside-menu-detail">
-                <h1 class='menu-title'>${menuTitle}</h1>
-                <h2 class='menu-price'>$${menuPrice}</h2>
-                <div class="menu-btn">
+                <h1 class='aside-menu-title'>${menuTitle}</h1>
+                <h2 class='aside-menu-price'>$${menuPrice}</h2>
+                <div class="aside-menu-btn">
                     <button class="decrement-btn">-</button>
-                    <h2 class="menu-count">1</h2>
+                    <h2 class="aside-menu-count">1</h2>
                     <button class="increment-btn">+</button>
                 </div>
             </div>
@@ -54,5 +53,5 @@ const addtoMenu = menuAside => {
         </div>
     `;
 
-    asideContent.append(asideMenu);
-}
+  asideContent.append(asideMenu);
+};
